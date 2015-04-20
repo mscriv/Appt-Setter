86f258d7a3124d402bccbe7edaf044bad1473090
Michael Scriven <bigscriv03@gmail.com>
2015-04-20 10:15:55 -0400

    Add start date validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,12 @@
 class Appointment < ActiveRecord::Base
-
   before_save :duration
 
 
-  'require csv'
+  validates :first_name, :last_name, :start_date,    presence: true
+  validates :start_time,    presence: true,
+            uniqueness: { scope: :start_date }
+  validate :expiration_date_cannot_be_in_the_past
+
 
   def self.import(file)
     CSV.foreach(file.path, headers:true) do |row|
@@ -11,15 +14,19 @@ class Appointment < ActiveRecord::Base
     end
   end
 
-  validates :start_date,    presence: true
-  validates :start_time,    presence: true,
-            uniqueness: { scope: :start_date }
+
+  def expiration_date_cannot_be_in_the_past
+    errors.add(:start_date, "can't be in the past") if
+        Date.strptime(self.start_date, '%m/%d/%Y') < Date.today
+  end
+
 
   def duration
     if self.end_time.present?
   ((self.end_time.gsub(':', '.').to_f - self.start_time.gsub(':', '.').to_f) *100).round(0)
     end
   end
+
 
 
 end
