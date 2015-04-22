0ba1d272541c3087d8c73ba867906c6264d086b6
Michael Scriven <bigscriv03@gmail.com>
2015-04-22 16:51:35 -0400

    Removed test directory

--- test/helpers/appointment_imports_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class AppointmentImportsHelperTest < ActionView::TestCase
-end
--- test/models/appointment_import_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AppointmentImportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
